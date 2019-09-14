d8a301a4199c73918b985ac36d886429c9ebd585
mantium <karol.zieniewicz@gmail.com>
2019-09-14 20:20:42 +0200

    corrected after first CR

--- src/tests/store/activities/reducer.test.js
+++ src/tests/store/activities/reducer.test.js
@@ -7,77 +7,79 @@ import {
   EDIT_ACTIVITY,
 } from '../../../store/activities/actionTypes';
 
-test('should set default activities state', () => {
-  const initAction = { type: '@@INIT' };
-  const state = activitiesReducer(undefined, initAction);
-  expect(state).toEqual({
-    activities: {
-      items: [],
-    },
+describe('activities reducer', () => {
+  it('should set default activities state', () => {
+    const initAction = { type: '@@INIT' };
+    const state = activitiesReducer(undefined, initAction);
+    expect(state).toEqual({
+      activities: {
+        items: [],
+      },
+    });
   });
-});
 
-test('should add an activity', () => {
-  const action = {
-    type: ADD_ACTIVITY,
-    activity,
-  };
-  const state = activitiesReducer(testState, action);
-  expect(state).toEqual({
-    activities: {
-      items: [...testState.activities.items, activity],
-    },
+  it('should add an activity', () => {
+    const action = {
+      type: ADD_ACTIVITY,
+      activity,
+    };
+    const state = activitiesReducer(testState, action);
+    expect(state).toEqual({
+      activities: {
+        items: [...testState.activities.items, activity],
+      },
+    });
   });
-});
 
-test('should remove activity by id', () => {
-  const id = '3';
-  const action = {
-    type: REMOVE_ACTIVITY,
-    id,
-  };
-  const state = activitiesReducer(testState, action);
-  expect(state).toEqual({
-    activities: {
-      items: [...testState.activities.items.filter(activityItem => activityItem.id !== id)],
-    },
+  it('should remove activity by id', () => {
+    const id = '3';
+    const action = {
+      type: REMOVE_ACTIVITY,
+      id,
+    };
+    const state = activitiesReducer(testState, action);
+    expect(state).toEqual({
+      activities: {
+        items: [...testState.activities.items.filter(activityItem => activityItem.id !== id)],
+      },
+    });
   });
-});
 
-test('should not remove activities when id not found', () => {
-  const id = '123';
-  const action = {
-    type: REMOVE_ACTIVITY,
-    id,
-  };
-  const state = activitiesReducer(testState, action);
-  expect(state).toEqual(testState);
-});
+  it('should not remove activities when id not found', () => {
+    const id = '123';
+    const action = {
+      type: REMOVE_ACTIVITY,
+      id,
+    };
+    const state = activitiesReducer(testState, action);
+    expect(state).toEqual(testState);
+  });
 
-test('should edit an activity', () => {
-  const { id } = testState.activities.items[1];
-  const name = 'playing video games';
-  const action = {
-    type: EDIT_ACTIVITY,
-    id,
-    updates: {
-      name,
-    },
-  };
-  const state = activitiesReducer(testState, action);
-  expect(state.activities.items[1].name).toBe(name);
-});
+  it('should edit an activity', () => {
+    const { id } = testState.activities.items[1];
+    const name = 'playing video games';
+    const action = {
+      type: EDIT_ACTIVITY,
+      id,
+      updates: {
+        name,
+      },
+    };
+    const state = activitiesReducer(testState, action);
+    expect(state.activities.items[1].name).toBe(name);
+  });
 
-test('should not edit an activity if id not found', () => {
-  const id = 'not existing id';
-  const name = 'playing video games';
-  const action = {
-    type: EDIT_ACTIVITY,
-    id,
-    updates: {
-      name,
-    },
-  };
-  const state = activitiesReducer(testState, action);
-  expect(state).toEqual(state);
+  it('should not edit an activity if id not found', () => {
+    const id = 'not existing id';
+    const name = 'playing video games';
+    const action = {
+      type: EDIT_ACTIVITY,
+      id,
+      updates: {
+        name,
+      },
+    };
+    const state = activitiesReducer(testState, action);
+    expect(state).toEqual(state);
+  });
 });
