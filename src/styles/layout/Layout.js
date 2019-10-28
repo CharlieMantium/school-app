1eab6cfdf5e2059dfd7c8a8dbe05f06f70318b0f
CharlieMantium <karol.zieniewicz@gmail.com>
2019-10-28 18:32:47 +0100

    replaced font files with link

--- src/styles/layout/Layout.js
+++ src/styles/layout/Layout.js
@@ -2,22 +2,10 @@ import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 import PropTypes from 'prop-types';
 import { Normalize } from 'styled-normalize';
-import Cabin from '../../../public/fonts/Cabin/Cabin-Regular.ttf';
-import Satisfy from '../../../public/fonts/Satisfy/Satisfy-Regular.ttf';
 
 const GlobalStyle = createGlobalStyle`
   html {
     box-sizing: border-box;
-		@import url(${Cabin});
-		@import url(${Satisfy});
-		@font-face {
-			font-family: Cabin;
-			src: url(${Cabin});
-		}
-		@font-face {
-			font-family: Satisfy;
-			src: url(${Satisfy});
-		}
 	};
 	body {
 		background-image: url('/images/background/background.jpg');
