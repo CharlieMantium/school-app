adf19034512a3a2a2c7a4269d80b11087432d2fc
CharlieMantium <karol.zieniewicz@gmail.com>
2019-11-04 22:21:27 +0100

    corrected after second CR

--- src/styles/elements/Heading.js
+++ src/styles/elements/Heading.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 import { colors, effects, spacing } from '../base/base';
 
-export const Heading = styled.p`
+export const Heading = styled.h2`
   color: ${colors.black};
   margin: ${spacing.mSize} auto;
   text-shadow: ${effects.outline(colors.white)};
